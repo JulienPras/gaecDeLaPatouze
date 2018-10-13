4cc6d68b71c6dadfc5fa8ea63042e41cb82a958e
Sylvain Gourier <sylvain.gourier@macroprog.com>
2018-10-13 17:53:06 +0200

    Upd: mailer configuration

--- mailer.php
+++ mailer.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+require_once 'vendor/autoload.php';
+
 const RECAPTCHA_SECRET = "6Le-RW4UAAAAANy2b7-NI2hzG5S73tDCg7KJ25-u";
 const RECAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify";
 const RECEIVER_MAIL = "julien.pras@gmail.com";
@@ -8,12 +12,14 @@ $data = array(
     'secret' => RECAPTCHA_SECRET,
     'response' => $_POST["g-recaptcha-response"]
 );
+
 $options = array(
     'http' => array (
         'method' => 'POST',
         'content' => http_build_query($data)
     )
 );
+
 $context  = stream_context_create($options);
 $verify = file_get_contents(RECAPTCHA_URL, false, $context);
 $captchaSsuccess = json_decode($verify);
@@ -30,9 +36,20 @@ if ($captchaSsuccess->success == true)
     $subject = stripslashes($subject);
     $from = stripslashes($from);
 
-    $message = "Name: ".$name."\n".$message;
-    $message = "From: ".$from."\n".$message;
-    $success = mail(RECEIVER_MAIL, 'Contact Gaec-de-la-Patouze.fr : '.$subject, $message, "From: $from");
+    $transport = (new Swift_SmtpTransport('macroprog.com', 465,'ssl'))
+        ->setUsername('contact@gaec-de-la-patouze.fr')
+        ->setPassword('lBe~1r44')
+    ;
+
+    $mailer = new Swift_Mailer($transport);
+
+    $message = (new Swift_Message('Contact gaec-de-la-patouze.fr'))
+        ->setFrom(['contact@gaec-de-la-patouze.fr' => 'Contact Gaec de la Patouze'])
+        ->setTo(RECEIVER_MAIL)
+        ->setBody($message)
+    ;
+
+    $success = $mailer->send($message);
 
     if($success)
     {
